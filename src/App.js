d227c711dc27e5d4de9427826f97ae867ec67231
SunandanBose <sunandanbose17@gmail.com>
2021-06-03 09:54:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css';
+import Particles from "./Particles.js";
+import Email from './images/Email.png';
+import Facebook from './images/Facebook.png';
+import Github from './images/GitHub.png';
+import LinkedIn from './images/LinkedIn.png';
+import Resume from './images/Resume.png';
+import CV from'./files/Resume.pdf';
+
+const styles = {
+  root: {
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    height: "100vh",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute"
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <Particles>
+        
+      </Particles>
+      <div className="title" >
+          <span className="name">Sunandan Bose</span>
+          <span className="description">Programmer - Web Developer - Engineer</span>
+          <div className="icontray">
+            <div>
+              <a aria-label= "LinkedInIcon" href="https://www.linkedin.com/in/sunandan-bose/" ><img className="icon" alt="linkedIn" src={LinkedIn}/></a>
+            </div>
+            <div>
+              <a aria-label= "FacebookIcon" href="https://www.facebook.com/sunandan.bose.3" ><img className="icon" alt="Facebook" src={Facebook}/></a>
+            </div>
+            <div>
+              <a aria-label= "GithubIcon" href="https://github.com/SunandanBose" ><img className="icon" alt="Github" src={Github}/></a>
+            </div>
+            <div>
+              <a aria-label= "EmailIcon" href="mailto:sunandanbose17@gmail.com" ><img className="icon" alt="Email" src={Email}/></a>
+            </div>
+            <div>
+              <a aria-label= "ResumeIcon" href={CV} download="Resume"><img className="icon" alt="Resume" src={Resume}/></a>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
