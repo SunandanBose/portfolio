7736cfc272101e13eb1bc34238f3cd1ee3136503
SunandanBose <sunandanbose17@gmail.com>
2021-06-03 12:32:45 +0530

    Modify Description

--- src/MyStory.js
+++ src/MyStory.js
@@ -10,7 +10,7 @@ function MyStory() {
             <img className = "profile photo" src={profilePic} alt="Avatar" />
             <br />
             <span className="profile aboutme"> 
-            Application Developer with 5 years of experience in Adobe.
+            Software Developer with 5 years of total experience. Currently working in Adobe.
             I have worked on various tools and technologies to deliver
             highly scalable solutions for various clients. Agile
             methodologies and extreme programming practitioner.
